78a0dc554ca676d2fe91579cece9e3f74c0a84c4
Vladimir Popov <vladimir.popov@xored.com>
2021-10-27 17:46:24 +0700

    Start using abstract sockets with memif
    Signed-off-by: Vladimir Popov <vladimir.popov@xored.com>

--- main.go
+++ main.go
@@ -200,7 +200,9 @@ func main() {
 			xconnect.NewServer(vppConn),
 			acl.NewServer(vppConn, config.ACLConfig),
 			mechanisms.NewServer(map[string]networkservice.NetworkServiceServer{
-				memif.MECHANISM: chain.NewNetworkServiceServer(memif.NewServer(vppConn)),
+				memif.MECHANISM: chain.NewNetworkServiceServer(
+					memif.NewServer(ctx, vppConn),
+				),
 			}),
 			connect.NewServer(
 				client.NewClient(
